d5f73a28b37e31cea9e2101b7a66928d0c5994ba
thaidev13639 <pnt0964413557@gmail.com>
2023-07-21 14:15:33 +0700

    first commit, update UX/UI for Home page, Customer page (Store page), add SCSS, add img, JS for payment table Customer

---
+++ customer/controller/main.js
@@ -0,0 +1,21 @@
+function domEle(id) {
+    return document.getElementById(id);
+};
+var isValid = true;
+function openTableCart() {
+    let tableCart = domEle("showCart");
+    if (isValid) {
+        tableCart.classList.add("show-cart");
+        isValid = false;
+    } else {
+        tableCart.classList.remove("show-cart");
+        isValid = true;
+    };
+};
+function continueShopping(){
+    let tableCart = domEle("showCart");
+    if(isValid === false){
+        tableCart.classList.remove("show-cart");
+        isValid = true;
+    };
+}
