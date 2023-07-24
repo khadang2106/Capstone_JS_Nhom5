17d1e75ec3de804471f1718c3abd4265b6ce7a10
KhaDang2106 <kha.dangvunhat@gmail.com>
2023-07-24 15:52:26 +0700

    Update JS: add/del/mod Product Fn

--- admin/controller/admin.js
+++ admin/controller/admin.js
@@ -1,33 +1,41 @@
-let api = new Service();
+import { Service } from '../services/api.js';
+import { Product } from '../models/product.js';
+
+const api = new Service();
 
 domId('btnAdd').onclick = function () {
   document.querySelector(
     '.modal-footer-btn'
-  ).innerHTML = `<button type="button" class="btn btn-info fn-btn">Add</button>`;
+  ).innerHTML = `<button type="button" class="btn btn-info fn-btn" onclick="addProduct()">Add</button>`;
+
+  resetFormValues();
 };
 
-function unavailableFn() {
-  domId('btnAdd').disabled = true;
-}
-function availableFn() {
-  domId('btnAdd').disabled = false;
-}
+window.unavailableFn = () => (domId('btnAdd').disabled = true);
+
+window.availableFn = () => (domId('btnAdd').disabled = false);
 
 const renderUI = (data) => {
-  const content = data.reduce((total, element) => {
+  const content = data.reduce((total, element, index) => {
     total += `
       <tr>
-          <td>${element.id}</td>
+          <td>${index + 1}</td>
           <td>${element.name}</td>
           <td>${element.price}</td>
           <td>
-            <img src="${element.img}" class="card-img-top d-block mx-auto" style="width: 100px"/>
+            <img src="${
+              element.img
+            }" class="card-img-top d-block mx-auto" style="width: 100px"/>
           </td>
           <td>${element.desc}</td>
           <td>${element.type}</td>
           <td class="text-center">
-            <button type="button" class="btn btn-danger fn-btn mb-1"><i class="fa-solid fa-trash"></i>Delete</button>
-            <button type="button" class="btn btn-warning fn-btn"><i class="fa-solid fa-wrench"></i>Modify</button>
+            <button type="button" class="btn btn-danger fn-btn mb-1" onclick="delProduct('${
+              element.id
+            }')"><i class="fa-solid fa-trash"></i>Delete</button>
+            <button type="button" data-toggle="modal" data-target="#addingModal" class="btn btn-warning fn-btn" onclick="modProduct('${
+              element.id
+            }')"><i class="fa-solid fa-wrench"></i>Modify</button>
           </td>
         </tr>
     `;
@@ -49,4 +57,149 @@ function getListProduct() {
     });
 }
 
-getListProduct();
+window.onload = () => getListProduct();
+
+const getFormValues = (id = '') => {
+  const name = domId('phoneName').value;
+  const price = domId('phonePrice').value;
+  const screen = domId('phoneScreen').value;
+  const backCamera = domId('phoneBackCam').value;
+  const frontCamera = domId('phoneFrontCam').value;
+  const img = domId('phoneImg').value;
+  const desc = domId('phoneDesc').value;
+  const type = domId('selcBrand').value;
+
+  const product = new Product(
+    id,
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    desc,
+    type
+  );
+
+  return product;
+};
+
+const fillFormValues = (
+  name = '',
+  price = '',
+  screen = '',
+  backCamera = '',
+  frontCamera = '',
+  img = '',
+  desc = '',
+  type = 'Select brand'
+) => {
+  domId('phoneName').value = name;
+  domId('phonePrice').value = price;
+  domId('phoneScreen').value = screen;
+  domId('phoneBackCam').value = backCamera;
+  domId('phoneFrontCam').value = frontCamera;
+  domId('phoneImg').value = img;
+  domId('phoneDesc').value = desc;
+  domId('selcBrand').value = type;
+};
+
+const resetFormValues = () => {
+  const name = domId('phoneName').value;
+  const price = domId('phonePrice').value;
+  const screen = domId('phoneScreen').value;
+  const backCamera = domId('phoneBackCam').value;
+  const frontCamera = domId('phoneFrontCam').value;
+  const img = domId('phoneImg').value;
+  const desc = domId('phoneDesc').value;
+  const type = domId('selcBrand').value;
+
+  if (
+    name !== '' &&
+    price !== '' &&
+    screen !== '' &&
+    backCamera !== '' &&
+    frontCamera !== '' &&
+    img !== '' &&
+    desc !== '' &&
+    type !== 'Select brand'
+  ) {
+    fillFormValues();
+  }
+};
+
+/**
+ * Add Product
+ */
+window.addProduct = () => {
+  const product = getFormValues();
+
+  const promise = api.addProductApi(product);
+  promise
+    .then(() => {
+      getListProduct();
+      document.querySelectorAll('.modal-footer button')[1].click();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+/**
+ * Delete Product
+ */
+window.delProduct = (id) => {
+  const promise = api.delProductApi(id);
+  promise
+    .then((result) => {
+      alert(`${result.data.name} has been deleted!`);
+      getListProduct();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+/**
+ * Update Product
+ */
+//Modify Product
+window.modProduct = (id) => {
+  document.querySelector(
+    '.modal-footer-btn'
+  ).innerHTML = `<button type="button" class="btn btn-warning fn-btn" onclick="updateProduct('${id}')">Update</button>`;
+
+  const promise = api.getProductById(id);
+  promise
+    .then((result) => {
+      fillFormValues(
+        result.data.name,
+        result.data.price,
+        result.data.screen,
+        result.data.backCamera,
+        result.data.frontCamera,
+        result.data.img,
+        result.data.desc,
+        result.data.type
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+//Update Product
+window.updateProduct = (id) => {
+  const product = getFormValues(id);
+
+  const promise = api.updateProductApi(product);
+  promise
+    .then(() => {
+      getListProduct();
+      alert(`Your product has been updated successfully!`);
+      document.querySelectorAll('.modal-footer button')[1].click();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
--- admin/services/api.js
+++ admin/services/api.js
@@ -1,10 +1,48 @@
-function Service() {
-  this.getListProductApi = function () {
-    let promise = axios({
+export class Service {
+  getListProductApi() {
+    const promise = axios({
       url: 'https://64bb72405e0670a501d7082f.mockapi.io/Products',
       method: 'GET',
     });
 
     return promise;
-  };
+  }
+
+  addProductApi(obj) {
+    const promise = axios({
+      url: 'https://64bb72405e0670a501d7082f.mockapi.io/Products',
+      method: 'POST',
+      data: obj,
+    });
+
+    return promise;
+  }
+
+  delProductApi(id) {
+    const promise = axios({
+      url: `https://64bb72405e0670a501d7082f.mockapi.io/Products/${id}`,
+      method: 'DELETE',
+    });
+
+    return promise;
+  }
+
+  getProductById(id) {
+    const promise = axios({
+      url: `https://64bb72405e0670a501d7082f.mockapi.io/Products/${id}`,
+      method: 'GET',
+    });
+
+    return promise;
+  }
+
+  updateProductApi(obj) {
+    const promise = axios({
+      url: `https://64bb72405e0670a501d7082f.mockapi.io/Products/${obj.id}`,
+      method: 'PUT',
+      data: obj,
+    });
+
+    return promise;
+  }
 }
